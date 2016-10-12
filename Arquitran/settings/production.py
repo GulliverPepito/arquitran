6ae501e293e8a1eab416a2eab5eb5417387f3423
Patricio López <lopezjuripatricio@gmail.com>
2016-10-12 01:25:26 -0300

    Setup Docker and server static files

--- Arquitran/settings/production.py
+++ Arquitran/settings/production.py
@@ -4,13 +4,15 @@ import os
 SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.getenv('DEBUG', False)
 
+ALLOWED_HOSTS = ['*']
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.environ.get('POSTGRES_NAME'),
         'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': os.environ.get('POSTGRES_HOST'),
-        'PORT': os.environ.get('POSTGRES_PORT'),
+        'HOST': os.getenv('POSTGRES_HOST', 'postgresql'),
+        'PORT': os.getenv('POSTGRES_PORT', ''),
     }
 }
